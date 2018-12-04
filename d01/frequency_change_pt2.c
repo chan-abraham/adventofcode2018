7ebe9ca4963338c3f25ea3490302af503ef47f09
chan-abraham <abe.chanowitz@gmail.com>
2018-12-04 09:33:44 -0800

    added comments to frequency_change_pt2.c

--- d01/frequency_change_pt2.c
+++ d01/frequency_change_pt2.c
@@ -2,18 +2,13 @@
 //github: chan-abraham
 
 #include "frequency_change.h"
-/*
-void	printlist(t_node *head)
-{
-	t_node	*current;
 
-	current = head;
-	while (current != NULL)
-	{
-		printf("list says: result = %d\n", current->save);
-		current = current->next;
-	}
-}
+/*
+** This function adds a new calculated result to the end of the linked list
+** Because of the way linked lists work (to get to the last element you have
+** to iterate through every element before it), I also used this opportunity
+** to check to see if the result already happened. If so, it sets the checker
+** variable to 1. In the end, it returns the checker int.
 */
 int		ft_listaddend(t_node *head, int result)
 {
@@ -34,14 +29,20 @@ int		ft_listaddend(t_node *head, int result)
 	current->next = (t_node *)malloc(sizeof(t_node));
 	current->next->save = result;
 	current->next->next = NULL;
-/*	printf("beginlist\n");
-	printlist(head);
-	printf("endlist\n");	*/
 	return (checker);
 }
 
+/*
+** This function is where the file is read and the calculated results get sent
+** to be saved to a linked list. In that saving process, it gets checked to see
+** if a repeat happens, and if it did, checker gets returned as 1, otherwise
+** checker gets returned as 0, signaling the main function that it needs to
+** continue through the process of reading and calculating the file again.
+*/
+
 int		read_calc(char *filename, t_node *head)
 {
+	//variable initialization
 	int		fd;
 	int		number;
 	char	*line;
@@ -51,32 +52,51 @@ int		read_calc(char *filename, t_node *head)
 
 	result = 0;
 	current = head;
-		fd = -1;
+	fd = -1;
 	number = 0;
 	checker = 0;
+	//opens the file
 	if ((fd = open(filename, O_RDONLY)) < 0)
 	{
 		ft_putendl("Error: Read");
 		return (0);
 	}
+	//uses get_next_line function, which reads the file line by line. stops
+	//looping when checker is returned as 1
 	while (get_next_line(fd, &line) && !checker)
 	{
+		//iterates to the end of the linked list
 		while (current->next != NULL)
 			current = current->next;
+		//atoi number from read line
 		number = ft_atoi(line);
+		//calculates result from the previous calculated result
 		result = current->save + number;
+		//puts result in end of list and checks for repetitition
 		checker = ft_listaddend(head, result);
-//		printf("The current Frequency is: %d\n", result);
+		//frees line
 		ft_memdel((void **)&line);
 	}
 	close(fd);
 //	printlist(head);
 	return (checker);
 }
-
+/*
+** main function and set up of the linked list
+*/
 int		main(int argc, char **argv)
 {
+	/*
+	** variable and linked list initialization
+	** the index here is not necessary for this assignment, but its just for
+	** my curiosity
+	*/
 	int		index;
+	/*
+	** checker is the variable to determine if a result has been found 0 for no,
+	** and 1 for yes. it is the main variable being passed around to check if the
+	** re-read needs to continue or it can stop (because a solution has been found)
+	*/
 	int		checker;
 	t_node	*head;
 
@@ -86,10 +106,15 @@ int		main(int argc, char **argv)
 	head->next = NULL;
 	index = 0;
 	checker = 0;
+	//sends the filename to be read, and it will continue doing this until
+	//checker returns 1 (or something else not zero, but that won't happen)
 	while (!checker && argc)
 	{
 		checker = read_calc(argv[1], head);
 		index++;
+		//just some extra info for me to know how many time the file needed to be
+		//read for the result to be found. in my given input, it had to read the
+		//file 143 times before finding the result
 		printf("%d\n", index);
 	}
 	return (0);;
